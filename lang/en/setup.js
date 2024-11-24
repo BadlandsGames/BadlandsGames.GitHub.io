e98dab78a49e2bd3d79897dda86ad845b5c8169b
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-11-24 13:26:03 -0700

    Update setup.js

--- lang/en/setup.js
+++ lang/en/setup.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     $("#bad_ajax_header").load("https://badlandsgames.github.io/lang/en/header.html");
-    $("#bad_ajax_header").load("https://badlandsgames.github.io/lang/en/footer.html");
+    $("#bad_ajax_footer").load("https://badlandsgames.github.io/lang/en/footer.html");
 });
