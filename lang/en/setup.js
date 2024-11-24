413df53fb475a9e824e69f2108ad1fac7dc1ef18
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-11-24 12:31:47 -0800

    made changes

--- lang/en/setup.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-    $("#bad_ajax_header").load("https://badlandsgames.github.io/lang/en/header.html");
-    $("#bad_ajax_footer").load("https://badlandsgames.github.io/lang/en/footer.html");
-});
